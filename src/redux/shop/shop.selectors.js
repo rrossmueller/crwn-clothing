046caa46c4e1181f81cf58efe7ce0726049a5d2d
Rossmueller, Ralf <ralf.rossmueller@rbhome.de>
2020-06-09 21:09:22 +0200

    Add asynchronous redux and introduce container components

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -18,3 +18,12 @@ export const selectCollection = collectionsUrlParam =>
         collections => collections ? collections[collectionsUrlParam] : null
     );
 
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
+);
